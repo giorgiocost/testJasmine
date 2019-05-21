0dbcdad150d5c95f12cd4cff4f260cfc534ed558
giorgiocost <gegiorgio1991@gmail.com>
2019-05-20 22:44:46 -0300

    utilizando toBe

--- jasmine-standalone-3/spec/suiteSpec.js
+++ jasmine-standalone-3/spec/suiteSpec.js
@@ -1,5 +1,15 @@
-describe("Operação de adição", () => {
-    it("deve garantir que 1+9 = 10",()=>{
+describe("Comparador toBe", () => {
+    it("deve validar o uso do toBe", ()=>{
+        expect(somar(9, 1)).toBe(10)
+    });
 
+    it("deve validar que os 2 obj não são iguais", () => {
+        let obj1 = { valor: true}
+        let obj2 = { valor: true}
+        expect(obj1).not.toBe(obj2)
     });
 });
+
+function somar( x ,y ){
+    return x+y
+}
